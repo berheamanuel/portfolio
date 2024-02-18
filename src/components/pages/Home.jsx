640cd6265448f3c692d1d33eedc8e7dc0997dc0e
Amanuel <amanueleman@gmail.com>
2024-02-17 22:56:58 -0500

    Home page created

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+      <div>
+        <p>Full Stack Web Developer</p> 
+        <h1>Hi, I am Amanuel Berhe</h1>
+        <img 
+        alt="self-photo"
+        className="img-fluid"
+        src="./assets/Images/my-photo.jpg" 
+        />        
+      </div>
+    );
+  }
+  
