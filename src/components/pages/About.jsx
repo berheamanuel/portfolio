f211375e7f3320fdeac66bf431e4eaf5e722631b
Amanuel <amanueleman@gmail.com>
2024-02-17 22:44:10 -0500

    About content created

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I am a passionate full stack web developer with expertise in HTML, CSS,
+        and JavaScript, React, Node.js and various other web technologies. I
+        love creating beautiful and functional web applications that provide
+        great user experiences.
+      </p>
+    </div>
+  );
+}
