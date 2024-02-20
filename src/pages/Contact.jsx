122d988e183370cf982e7e31f7dd2156bfbdca89
Amanuel <amanueleman@gmail.com>
2024-02-19 21:32:27 -0500

    Added validation input logic to contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,4 @@
+import { object } from "prop-types";
 import React, { useState } from "react";
 
 function Contact() {
@@ -12,6 +13,8 @@ function Contact() {
     email: "",
     message: "",
   });
+
+  const[submitted, setSubmitted] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -39,43 +42,71 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
+    let valid = true;
+    const{ email, name, message } = formData;
+    
+      if (!name || !email || !message) 
+      {
+        setError({email: !formData.email ? "Email is required" : '', name: !formData.name ? "Name is required" : '', 
+        message: !formData.message ? "Message is required" : ''
+      });
+        valid = false;
+      }
+    
+    if (!valid) return;
+
+    setSubmitted(true);
   };
 
   return (
     <div>
       <h1>Contact</h1>
+      {!submitted ? (
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
+        <div className="row g-3 align-items-center">
+          <label className="form-label" htmlFor="name">Name:</label>
+          <div className="col-auto">
           <input
+            className="form-control"
             type="text"
+            name="name"
             id="name"
             value={formData.name}
             onChange={handleChange}
           />
+          </div>
             {error.name && <span>{error.name}</span>}
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div className="row g-3 align-items-center">
+          <label className="form-label" htmlFor="email">Email:</label>
+          <div className="col-auto">
           <input
+            className="form-control"
             type="email"
+            name="email"
             id="email"
             value={formData.email}
             onChange={handleChange}
           />
+          </div>
           {error.email && <span>{error.email}</span>}
         </div>
-        <div>
-          <label htmlFor="message">Message:</label>
+        <div className="row g-3 align-items-center">
+          <label className="form-label" htmlFor="message">Message:</label>
+          <div className="col-auto">
           <textarea
+            className="form-control"
             id="message"
+            name="message"
             value={formData.message}
             onChange={handleChange}
           ></textarea>
+          </div>
             {error.message && <span>{error.message}</span>}
         </div>
         <button type="submit">Submit</button>
       </form>
+      ) : (<p>Form submitted!</p>)}
     </div>
   );
 }
