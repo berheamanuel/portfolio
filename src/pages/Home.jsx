425e9358025bd7a0f74c11cf990ee2fdb2e893ce
Amanuel <amanueleman@gmail.com>
2024-06-25 16:27:17 -0400

    added hero and background image to the home page, styled the home page using bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
+import profileImage from "../assets/hero-img-1.jpg";
+import './Home.css';
+
 export default function Home() {
-    return (
-      <div>
-        <p>Full Stack Web Developer</p> 
-        <h1>Hi, I am Amanuel Berhe</h1>
-        <img 
-        alt="self-photo"
-        className="img-fluid"
-        src="./assets/Images/my-photo.jpg" 
-        />        
+  return (
+    <div className="hero d-flex align-items-center">
+      <div className="container text-center">
+        <h1 className="display-4">Hey, I'm Amanuel</h1>
+        <p className="lead">A Full Stack Web Developer</p>
+        <p className="hero-description">
+          I build responsive, high-performance web application with modern
+          technologies.
+        </p>
+        <a href="" className="btn btn-primary btn-sm">
+          View My Work
+        </a>
       </div>
-    );
-  }
-  
+
+      <div>
+        <img
+          className="img-fluid mb-3 mb-lg-0 profile-photo"
+          src={profileImage}
+          alt="self-photo"
+        />
+      </div>
+    </div>
+  );
+}
